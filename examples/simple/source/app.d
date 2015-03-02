f5ef93afeeda9e7dbd6fa7d69e4387d2e53d7188
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2015-03-02 21:22:16 +0300

    fix simple example

--- examples/simple/source/app.d
+++ examples/simple/source/app.d
@@ -20,7 +20,7 @@ void main()
     auto qmlApplicationEngine = new QQmlApplicationEngine;
     scope(exit) destroy(qmlApplicationEngine);
     qmlApplicationEngine.context().setContextProperty("myQObject", new QVariant(myQObject));
-    qmlApplicationEngine.load("view/window.qml");
+    qmlApplicationEngine.load("views/window.qml");
 
     app.exec();
   }
