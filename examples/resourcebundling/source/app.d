bbe86d408925c02f3eb1410fef981353dbe9a06d
Moritz Maxeiner <moritz@ucworks.org>
2015-12-06 01:32:35 +0100

    Add example showing how to bundle the main QML file inside of an external binary resource

---
+++ examples/resourcebundling/source/app.d
@@ -0,0 +1,24 @@
+import std.stdio;
+import dqml;
+import std.path : dirSeparator;
+
+void main()
+{
+    try
+    {
+        auto app = new QGuiApplication();
+        scope(exit) destroy(app);
+
+        auto engine = new QQmlApplicationEngine();
+        scope(exit) destroy(engine);
+
+        QResource.registerResource(app.applicationDirPath
+            ~ dirSeparator ~ "app.rcc");
+
+        engine.load(new QUrl("qrc:///app.qml"));
+
+        app.exec();
+    }
+    catch
+    {}
+}
