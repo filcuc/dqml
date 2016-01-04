8274c7dd807fb4fdd8423eeea4a5abb93941f548
Filippo Cucchetto <filippocucchetto@gmail.com>
2016-01-04 11:09:24 +0100

    Reverted changes to examples

--- examples/helloworld/source/app.d
+++ examples/helloworld/source/app.d
@@ -1,28 +1,20 @@
 import std.stdio;
 import std.traits;
 import dqml;
-import dqml.dothersideinterface;
-import dqml.qobjectgenerators;
-
-
-@QtProperty(string.stringof, "name", "name", "setName", "nameChanged")
-class QObject2Subclass : QObject
-{
-  mixin Q_OBJECT;
-
-  @QtSlot() public string GetName() { return m_name; }
-  @QtSlot() public void SetName(string name) { m_name = name; }
-  @QtSignal() public void nameChanged(string name);
-
-  private string m_name;
-}
 
 void main()
 {
-  try {
-    auto temp2 = new QObject2Subclass();
-    temp2.nameChanged("prova");
-    writeln(temp2.metaObject() is null);
-  }
-  catch {}
+    try
+    {
+        auto app = new QGuiApplication();
+        scope(exit) destroy(app);
+
+        auto engine = new QQmlApplicationEngine();
+        scope(exit) destroy(engine);
+        engine.load("app.qml");
+
+        app.exec();
+    }
+    catch
+    {}
 }
